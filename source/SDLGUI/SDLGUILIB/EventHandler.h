2209f696b4c7bf0a04b5e9de3a7683b46f9d7668
soeren <soeren.graf@gmail.com>
2015-12-30 13:18:28 +0100

    missing file?

--- source/SDLGUI/SDLGUILIB/EventHandler.h
+++ source/SDLGUI/SDLGUILIB/EventHandler.h
@@ -2,9 +2,12 @@
 
 #include "SDL.h"
 #include "SDLGUILIB_API.h"
+#include <vector>
 
 namespace sgl
 {
+	class Window;
+
 	SDLGUILIB_API int GlobalInitialize();
 	SDLGUILIB_API int GlobalCleanup();
 	SDLGUILIB_API void HandleEvent(SDL_Event& e);
@@ -17,6 +20,8 @@ namespace sgl
 		SDL_Event& process(SDL_Event& e);
 
 	private:
+		std::vector<Window*> rootWindows;
+
 		EventHandler& operator=(const EventHandler&) = delete;
 		EventHandler(const EventHandler&) = delete;
 	};
